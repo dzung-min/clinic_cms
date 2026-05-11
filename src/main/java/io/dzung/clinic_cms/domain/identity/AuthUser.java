33b73807576f694c29aeaf4765a5c2e5e954f64a
Dzung Nguyen <dzung_min@yahoo.com>
2026-05-12 02:38:26 +0900

    fix: change in AuthUser, use Lombok @RequireArgsConstructor instead of manual constructor

--- src/main/java/io/dzung/clinic_cms/domain/identity/AuthUser.java
+++ src/main/java/io/dzung/clinic_cms/domain/identity/AuthUser.java
@@ -7,12 +7,11 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-public class AuthUser implements UserDetails {
-    private User user;
+import lombok.RequiredArgsConstructor;
 
-    public AuthUser(User user) {
-        this.user = user;
-    }
+@RequiredArgsConstructor
+public class AuthUser implements UserDetails {
+    private final User user;
 
     public User getUser() {
         return user;
